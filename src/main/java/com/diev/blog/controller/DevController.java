9b6aa23d685200201225625801260bf386675a1a
AMinSC <cdtjdcjf@gmail.com>
2024-08-11 20:45:14 +0900

    templates comment update

--- src/main/java/com/diev/blog/controller/DevController.java
+++ src/main/java/com/diev/blog/controller/DevController.java
@@ -1,4 +1,15 @@
 package com.diev.blog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dev")
 public class DevController {
+
+    @GetMapping("/")
+    public String devHome() {
+        return "dev/dev_home";
+    }
 }
